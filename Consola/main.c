5fc6a34535aef3d06b814a73cd0d2094627efc86
gonziesc <gonziesc@hotmail.com>
2017-04-06 23:24:58 -0300

    creados proyectos

---
+++ Consola/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include <commons/config.h>
+
+
+
+
+int main(int argc, char**argv){
+	printf("arranquemos, so \n");
+		//configuracion(t_archivoConfig, config, argv[1]);
+
+		return EXIT_SUCCESS;
+}
+
+//void configuracion(archivoConfigKernel *unArchivo, t_config* config, char dir[]){
+
+
+//}
