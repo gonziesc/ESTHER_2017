d637d94e8c65c83a43c9470d631f923104c79096
gonziesc <gonziesc@hotmail.com>
2017-04-08 16:39:50 -0300

    termiando config kernel

--- Kernel/main.c
+++ Kernel/main.c
@@ -4,7 +4,7 @@
 #include <commons/config.h>
 #include "abstracciones.c"
 
-
+void configuracion(archivoConfigKernel *, t_config* , char *);
 
 int main(int argc, char**argv){
 	archivoConfigKernel* t_archivoConfig = malloc(sizeof(archivoConfigKernel));
@@ -14,16 +14,17 @@ int main(int argc, char**argv){
 
 	return EXIT_SUCCESS;
 }
-void configuracion(archivoConfigKernel *unArchivo, t_config* config, char dir[]){
+void configuracion(archivoConfigKernel *unArchivo, t_config* config, char *dir){
+	int i = 0;
 	config = config_create(dir);
 	unArchivo->PUERTO_CPU = config_get_int_value(config, "PUERTO_CPU");
 	printf("PUERTO_CPU: %d\n", unArchivo->PUERTO_CPU);
 	unArchivo->PUERTO_PROG = config_get_int_value(config, "PUERTO_PROG");
 	printf("PUERTO_PROG: %d\n", unArchivo->PUERTO_PROG);
-	unArchivo->IP_MEMORIA  = config_get_int_value(config, "IP_MEMORIA");
-	printf("IP_MEMORIA: %d\n", unArchivo->IP_MEMORIA);
-	unArchivo->IP_FS = config_get_int_value(config, "IP_FS");
-	printf("IP_FS: %d\n", unArchivo->IP_FS);
+	unArchivo->IP_MEMORIA  = config_get_string_value(config, "IP_MEMORIA");
+	printf("IP_MEMORIA: %s\n", unArchivo->IP_MEMORIA);
+	unArchivo->IP_FS = config_get_string_value(config, "IP_FS");
+	printf("IP_FS: %s\n", unArchivo->IP_FS);
 	unArchivo->PUERTO_FS  = config_get_int_value(config, "PUERTO_FS");
 	printf("PUERTO_FS: %d\n", unArchivo->PUERTO_FS);
 	unArchivo->QUANTUM = config_get_int_value(config, "QUANTUM");
@@ -36,8 +37,25 @@ void configuracion(archivoConfigKernel *unArchivo, t_config* config, char dir[])
 	printf("GRADO_MULTIPROG:%d\n", unArchivo->GRADO_MULTIPROG);
 	unArchivo->STACK_SIZE  = config_get_int_value(config, "STACK_SIZE");
 	printf("STACK_SIZE:%d\n", unArchivo->STACK_SIZE);
-	//unArchivo->SEM_IDS = config_get_array_value(config, "SEM_IDS");
-	//unArchivo->SEM_INIT  = config_get_array_value(config, "SEM_INIT");
-	//unArchivo->SHARED_VARS = config_get_array_value(config, "SHARED_VARS");
-	//ojo ips, solo impre el primero. ojo arrays.
+	unArchivo->SEM_IDS = config_get_array_value(config, "SEM_IDS");
+	unArchivo->SEM_INIT  = config_get_array_value(config, "SEM_INIT");
+	unArchivo->SHARED_VARS = config_get_array_value(config, "SHARED_VARS");
+	while(unArchivo->SEM_IDS[i] != NULL)
+	{
+		printf("SEM_IDS:%s\n", unArchivo->SEM_IDS[i]);
+		i++;
+	}
+	i=0;
+	while(unArchivo->SEM_INIT[i] != NULL)
+		{
+		unArchivo->SEM_INIT[i] = atoi((int)unArchivo->SEM_INIT[i]);
+			printf("SEM_INIT:%d\n", unArchivo->SEM_INIT[i]);
+			i++;
+	}
+	i=0;
+		while(unArchivo->SHARED_VARS[i] != NULL)
+			{
+			printf("SHARED_VARS:%s\n", unArchivo->SHARED_VARS[i]);
+			i++;
+		}
 }
