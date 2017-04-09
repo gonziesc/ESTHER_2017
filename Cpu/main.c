1f99d75e98593fec282fa65f1e4c15aa1c2b65ea
gonziesc <gonziesc@hotmail.com>
2017-04-09 01:31:15 -0300

    multiservidor kernel y servidor consola para probar

--- Cpu/main.c
+++ Cpu/main.c
@@ -2,13 +2,29 @@
 #include<stdlib.h>
 #include <string.h>
 #include <commons/config.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 
 
 
 int main(int argc, char**argv){
-	printf("arranquemos, so \n");
+	//hacer config
 		//configuracion(t_archivoConfig, config, argv[1]);
+		char *ipKernel = "127.0.0.1";
+		int puertoKernel = 5000;
+
+		struct sockaddr_in direccionKernel;
+		direccionKernel.sin_family = AF_INET;
+		direccionKernel.sin_addr.s_addr = inet_addr(ipKernel);
+		direccionKernel.sin_port = htons(puertoKernel);
+
+			int cliente = socket(AF_INET, SOCK_STREAM, 0);
+			if (connect(cliente, (void*) &direccionKernel, sizeof(direccionKernel)) != 0) {
+				perror("No se pudo conectar");
+				return 1;
+			}
+			while(1){};
 
 		return EXIT_SUCCESS;
 }
--- Kernel/main.c
+++ Kernel/main.c
@@ -3,14 +3,119 @@
 #include <string.h>
 #include <commons/config.h>
 #include "abstracciones.c"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 void configuracion(archivoConfigKernel *, t_config* , char *);
 
 int main(int argc, char**argv){
+	struct sockaddr_in direccionCliente;
+	unsigned int tamanoDireccion;
+	 int fdmax;
+     int newfd;        // descriptor de socket de nueva conexión aceptada
+     char buf[256];    // buffer para datos del cliente
+     int nbytes;
+     int i, j;
+    fd_set master;   // conjunto maestro de descriptores de fichero
+	fd_set read_fds; // conjunto temporal de descriptores de fichero para select()
 	archivoConfigKernel* t_archivoConfig = malloc(sizeof(archivoConfigKernel));
 	t_config *config = malloc(sizeof(t_config));
 	printf("arranquemos, so \n");
 	configuracion(t_archivoConfig, config, argv[1]);
+
+	 FD_ZERO(&master);    // borra los conjuntos maestro y temporal
+	    FD_ZERO(&read_fds);
+
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	direccionServidor.sin_port = htons(5000);
+	 memset(&(direccionServidor.sin_zero), '\0', 8);
+
+		int servidor = socket(AF_INET, SOCK_STREAM, 0);
+
+		int activado = 1;
+		setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+
+		if (bind(servidor, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
+			perror("Falló el bind");
+			return 1;
+		}
+
+		printf("Estoy escuchando\n");
+		listen(servidor, 100);
+
+		//------------------------------
+
+
+		//int cliente = accept(servidor, (void*) &direccionCliente, &tamanoDireccion);
+
+		//printf("Kernel recibio una conexión en %d!!\n", cliente);
+
+	//	char* buffer = malloc(1000);
+		 FD_SET(servidor, &master);
+		        // seguir la pista del descriptor de fichero mayor
+		        fdmax = servidor; // por ahora es éste
+		 for(;;) {
+		            read_fds = master; // cópialo
+		            if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+		                perror("select");
+		                exit(1);
+		            }
+
+		            for(i = 0; i <= fdmax; i++) {
+		                           if (FD_ISSET(i, &read_fds)) { // ¡¡tenemos datos!!
+		                               if (i == servidor) {
+		                                   // gestionar nuevas conexiones
+		                            	   tamanoDireccion = sizeof(direccionCliente);
+		                                   if ((newfd = accept(servidor, (void*)&direccionCliente,
+		                                                                            &tamanoDireccion)) == -1) {
+		                                       perror("accept");
+		                                   } else {
+		                                       FD_SET(newfd, &master); // añadir al conjunto maestro
+		                                       if (newfd > fdmax) {    // actualizar el máximo
+		                                           fdmax = newfd;
+		                                       }
+		                                       printf("selectserver: new connection from %s on "
+		                                           "socket %d\n", inet_ntoa(direccionCliente.sin_addr), newfd);
+		                                   }
+		                               } else {
+		                                   // gestionar datos de un cliente
+		                                   if ((nbytes = recv(i, buf, sizeof(buf), 0)) <= 0) {
+
+		                                       // error o conexión cerrada por el cliente
+		                                       if (nbytes == 0) {
+		                                           // conexión cerrada
+		                                           printf("selectserver: socket %d hung up\n", i);
+		                                       } else {
+		                                           perror("recv");
+		                                       }
+		                                       close(i); // bye!
+		                                       FD_CLR(i, &master); // eliminar del conjunto maestro
+		                                   } else {
+		                                	   printf("Me llegaron %d bytes con %s\n", nbytes, buf);
+		                                       // tenemos datos de algún cliente
+		                                       for(j = 0; j <= fdmax; j++) {
+		                                           // ¡enviar a todo el mundo!
+		                                           if (FD_ISSET(j, &master)) {
+		                                               // excepto al listener y a nosotros mismos
+		                                               if (j != servidor && j != i) {
+		                                                   if (send(j, buf, nbytes, 0) == -1) {
+		                                                       perror("send");
+		                                                   }
+		                                               }
+		                                           }
+		                                       }
+		                                   }
+		                               }
+		                           }
+		            }
+		 }
+
+
 
 	return EXIT_SUCCESS;
 }
