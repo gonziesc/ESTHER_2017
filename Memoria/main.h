61afe24075c9e56080d89001643117045aa09f23
lautiallboys <you@example.com>
2017-06-17 19:55:52 -0300

    funciones de hash sin implementar

--- Memoria/main.h
+++ Memoria/main.h
@@ -16,6 +16,7 @@
 #include <serializador.h>
 #include <semaphore.h>
 #include <commons/log.h>
+#include <commons/collections/list.h>
 
 typedef struct {
 	int32_t id;
@@ -80,4 +81,16 @@ char* leerDeCache(int32_t , int32_t ,int32_t, int32_t );
 void escribirEnCache(int32_t , int32_t , int32_t , int32_t , char* );
 void almacenarFrameEnCache(int32_t , int32_t , char* , int32_t);
 int32_t buscarPosicionContenido(int32_t, int32_t );
+
+t_list** overflow;
+int CANTIDAD_DE_MARCOS;
+unsigned int calcularPosicion(int pid, int num_pagina);
+void inicializarOverflow(int cantidad_de_marcos);
+void agregarSiguienteEnOverflow(int pos_inicial, int nro_frame);
+int buscarEnOverflow(int indice, int pid, int pagina);
+void borrarDeOverflow(int pos_inicial, int frame);
+int esPaginaCorrecta(int pos_candidata, int pid, int pagina);
+
+
 #endif
+
